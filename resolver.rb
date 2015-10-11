ab336b743d98f81ee939ca7a4e20d9fea390e881
ludocracy <bansuki@gmail.com>
2015-10-10 19:43:55 -0500

    parameterization works for all arithmetic, boolean and some comparisons

--- resolver.rb
+++ resolver.rb
@@ -1,8 +1,227 @@
-module Resolver
-  require_relative 'symbolic_modified/symbolic'
-  require_relative 'debug'
-  include Symbolic
-  include Debug
+require 'symbolic'
+require 'dentaku/parser'
+require 'dentaku/calculator'
+require 'dentaku/ast/identifier'
+
+module Symbolic
+  include Comparable
+  #need to add support for boolean parameter operations!
+  def <=> arg
+    operator = caller[0]
+    if self.is_a?(Symbolic::Variable) || arg.is_a?(Symbolic::Variable)
+      0 if self.value == arg.value
+      Variable.new(name: self.value+operator+arg.value)
+    else
+      self.value <=> arg.value
+    end
+  end
+end
+
+module Resolver_wrapper
+  include Dentaku
+  include AST
+
+  #have to redefine whole class to override the one method below
+  class AST::Arithmetic < Operation
+    def initialize(*)
+      super
+    end
+
+    def type
+      :numeric
+    end
+
+    def value(context={})
+      l = cast(left.value(context))
+      r = cast(right.value(context))
+      l.public_send(operator, r)
+    end
+
+    private
+
+    def cast(value, prefer_integer=true)
+      return value unless value.respond_to?(:to_i)
+      v = Rational(value)
+      v = v.to_i if prefer_integer && v.to_i == v
+      v
+    end
+  end
+
+  class AST::And < Combinator
+    def value(context={})
+      case
+        when left.value === false, right.value === false then false
+        when left.is_a?(Parameter), right.value(context) === true then left.value
+        when right.is_a?(Parameter), left.value(context) === true then right.value
+        else left.value(context) && right.value(context)
+      end
+    end
+  end
+
+  class AST::Or < Combinator
+    def value(context={})
+      case
+        when left.value === true, right.value === true then true
+        when left.is_a?(Parameter), right.value(context) === false then left.value
+        when right.is_a?(Parameter), left.value(context) === false then right.value
+        else left.value(context) || right.value(context)
+      end
+
+    end
+  end
+
+
+  class AST::Division < Arithmetic
+    def value(context={})
+      d = cast(right.value(context), true)
+      raise ZeroDivisionError if d.respond_to?(:zero?) && d.zero?
+      n = cast(left.value(context))
+      cast(n/d)
+    end
+
+    def self.precedence
+      20
+    end
+  end
+
+  class Parameter < Identifier
+    def value(context={})
+      v = context[identifier]
+      case v
+        when Dentaku::AST::Node
+          v.value(context)
+        when NilClass
+          identifier[0..2] = '!' if identifier.length > 3 && identifier[0..2] == 'not'
+          Symbolic::Variable.new :name => identifier
+        else
+          v
+      end
+    end
+  end
+
+  #overriding Dentaku to use Resolver, not Calculator
+
+
+  #overriding Calculator to use Simplifier not Parser
+  class Resolver < Calculator
+    def ast(expression)
+      @ast_cache.fetch(expression) {
+        Simplifier.new(tokenizer.tokenize(expression)).parse.tap do |node|
+          node
+          @ast_cache[expression] = node if Dentaku.cache_ast?
+        end
+      }
+    end
+
+    def evaluate(expression, data={})
+      rewritten_expr = expression.dup
+      rewritten_expr.gsub!('||', 'or')
+      rewritten_expr.gsub!('&&', 'and')
+      rewritten_expr.gsub!('!false', 'true')
+      rewritten_expr.gsub!('!true', 'false')
+      rewritten_expr.gsub!('!', 'not')
+      evaluate!(rewritten_expr, data)
+    end
+
+    def evaluate!(expression, data={})
+      results = super(expression, data)
+      case
+        when results.respond_to?(:to_s) then results.to_s
+        when results.respond_to?(:name) then results.name
+        else results
+      end
+    end
+  end
+
+  #overriding Parser parse to replace unknown value Identifiers with Parameters
+  #the method is huge so had to replace it completely
+  class Simplifier < Parser
+    def parse
+      return Dentaku::AST::Nil.new if input.empty?
+
+      while token = input.shift
+        case token.category
+          when :numeric
+            output.push Dentaku::AST::Numeric.new(token)
+
+          when :logical
+            output.push Dentaku::AST::Logical.new(token)
+
+          when :string
+            output.push Dentaku::AST::String.new(token)
+
+          when :identifier
+            output.push Parameter.new(token)
+
+          when :operator, :comparator, :combinator
+            op_class = operation(token)
+
+            if op_class.right_associative?
+              while operations.last && operations.last < Dentaku::AST::Operation && op_class.precedence < operations.last.precedence
+                consume
+              end
+
+              operations.push op_class
+            else
+              while operations.last && operations.last < Dentaku::AST::Operation && op_class.precedence <= operations.last.precedence
+                consume
+              end
+
+              operations.push op_class
+            end
+
+          when :function
+            arities.push 0
+            operations.push function(token)
+
+          when :grouping
+            case token.value
+              when :open
+                if input.first && input.first.value == :close
+                  input.shift
+                  consume(0)
+                else
+                  operations.push Dentaku::AST::Grouping
+                end
+
+              when :close
+                while operations.any? && operations.last != Dentaku::AST::Grouping
+                  consume
+                end
+
+                lparen = operations.pop
+                fail "Unbalanced parenthesis" unless lparen == Dentaku::AST::Grouping
+
+                if operations.last && operations.last < Dentaku::AST::Function
+                  consume(arities.pop.succ)
+                end
+
+              when :comma
+                arities[-1] += 1
+                while operations.any? && operations.last != Dentaku::AST::Grouping
+                  consume
+                end
+
+              else
+                fail "Unknown grouping token #{ token.value }"
+            end
+
+          else
+            fail "Not implemented for tokens of category #{ token.category }"
+        end
+      end
+
+      while operations.any?
+        consume
+      end
+
+      unless output.count == 1
+        fail "Parse error"
+      end
+
+      output.first
+    end
+  end
 
   def operators current_node
     operators = {}
@@ -13,134 +232,10 @@ module Resolver
     end
     logics.each do |logic|
       operators[] = @logics[logic]
+    end
   end
 
   def default_logics
     [:string, :boolean, :arithmetic]
   end
-
-  #this is assuming we've been passed
-  def parameterize current_node, param_hash
-    @logic_hash = operators current_node
-    reference_node = current_node
-    current_node = reference_node.clone
-    changed = false
-    #get an array of parameterized xml nodes and loop through them
-    current_node.get_parameterized_xml_nodes.each do |parameterized_xml_node, paramized_string|
-      macro_strings = find_expr paramized_string
-      macro_strings.each do |macro_string|
-        STDERR.puts "found a macro_string: '#{macro_string}'!"
-        if param_hash.is_a? Hash
-          #looping through each known parameter from hash
-          param_hash.keys.each do |key|
-            STDERR.puts "swapping in value of '#{key.to_s}'!"
-            #replace parameters with values in given macro string
-            #need to add s
-            if macro_string[key.to_s]
-              changed ||= true
-              macro_string[key.to_s] = param_hash[key]
-            end
-          end
-        end
-        #simplify result, retaining unknown value parameters as strings
-        results = simplify macro_string
-        STDERR.puts "resolved to: '#{results[0]}'!"
-        parameterized_xml_node.content["@(#{macro_string})"] = results[0]
-        #add removal of returned cancelled parameters to template's run-time history
-        #record_remove results[1...-1]
-      end
-    end
-    if changed
-      current_node
-    else
-      reference_node
-    end
-  end
-
-  def find_expr string
-    macro_strings = []
-    fragments = string.split('@(')
-    fragments.each_with_index do |fragment, index|
-      if index == 0
-        next
-      end
-      d fragment
-      close_index = find_close_parens_index "(#{fragment}"
-      macro_strings << fragment[0...(close_index-1)]
-    end
-    d macro_strings
-    macro_strings
-  end
-
-  #had to create my own close parentheses finder
-  def find_close_parens_index string
-    #tracks current index of string
-    pos = 0
-    #tracks how many parentheses deep we're nested
-    parens_depth = 0
-    #looping through each char
-    string.split(//).each_with_index do |char, index|
-      case char
-        #found a close parenthesis
-        when ')'
-          #move down a level
-          parens_depth -= 1
-        #found an open parenthesis
-        when '('
-          #move up a level
-          parens_depth += 1
-        else
-      end
-      case parens_depth
-        #depth is 0 - we found it!
-        when 0
-          #return current char index
-          return index
-        #how'd we go negative?
-        when parens_depth < 0
-          #throw an error - this needs to generate an error if user does not correct!
-          throw :too_many_close_parens, 'parentheses error! too many close parentheses!'
-        else
-      end
-    end #end of string loop
-
-    #method should not get here unless it never found enough close parentheses
-    throw :too_many_open_parens, 'parentheses error! too many open parentheses!'
-  end
-
-  #uses Symbolic gem to leave parameters with unknown values intact while resolving remaining terms
-  #simplify result, retaining unknown value parameters as strings
-  #may want to expose this particular method because it's just so damn useful!
-  def simplify macro_string
-    #find potential parameters and remove duplicates
-    #***********need to screen for legal operators and methods here!!!
-    potential_vars = macro_string.scan(/\b[a-z][a-zA-Z0-9_]*/)
-    #remove duplicates
-    potential_vars.uniq!
-    #remove any that are actually operators
-    #declare them as symbolic vars and add '@' to each in macro string
-    potential_vars.to_a.each do |variable|
-      #have to convert into an instance variable name
-      instance_var_name = "@#{variable}"
-      #replace in macro string
-      macro_string.gsub! variable, instance_var_name
-      #declare as instance variable of type Symbolic::Variable
-      instance_variable_set(instance_var_name, Variable.new(:name => variable))
-    end
-    d "about to evaluate #{macro_string}"
-    #evaluate expression and return
-    result_str = (eval macro_string).to_s
-    #check for parameters that were cancelled out and add to array
-    cancelled_parameters = []
-    potential_vars.each do |var|
-      #result string no longer contains variable! return it with result!
-      if !result_str.include? var
-        cancelled_parameters << var
-      end
-    end
-    #return both result and array of cancelled parameters
-    return result_str, cancelled_parameters
-  end
-
-  private :parameterize, :find_close_parens_index, :find_expr, :default_logics
 end
--- test.rb
+++ test.rb
@@ -1,8 +1,6 @@
-module Test
-  begin
-  relatives = %w(dentaku/lib/dentaku)
-  requires = %w()
-  includes = %w(Dentaku)
+begin
+  relatives = %w(symbolic dentaku)
+  includes = %w(Symbolic Dentaku)
   requires.each do |required|
     STDERR.puts "requiring #{required}"
     require required
@@ -15,70 +13,8 @@ module Test
     STDERR.puts "including #{included}"
     include Module.const_get(included)
   end
-   end#includes, requires, etc
-  #***********START**************
+end #includes, requires, etc
 
-  def self.start
-    cleared = 5
-    regression_tests = {}
-    dev_tests = {}
-    test_hash0=[
-    ]
-
-    test_hash=[
-        "var" => "var",
-        "2+2" =>  "4",
-        "2*2" =>  "4",
-        "var+2" =>  "var+2",
-        "var*2" =>  "var*2",
-        "var/2" =>  "var/2",
-        "2+var" =>  "2+var",
-        "2*var" =>  "2*var",
-        "2/var" =>  "2/var",
-        "4-2+var" =>  "2+var",
-        "var+4-2" =>  "var+2",
-        "var+var" =>  "var+var",
-        "var*var" =>  "var*var",
-        "var/var" =>  "var/var",
-        "var*2-1" =>  "var*2-1",
-        "var-2*1" =>  "var+-2",
-        "2-1*var" =>  "2-1*var",
-        "2-1/var" => "2-1/var",
-        "var*2/1" =>  "var*2",
-        "var/2-1" =>  "var/2-1",
-        "2/1*var" =>  "2*var",
-        "2/1-var" => "2-var",
-        "var/2*4" => "var*2", #****************breaking from here
-        "var*4/2" => "var*2",
-        "var-2+var" =>  "var+-2+var",
-        "var-2*var" =>  "var+-2*var",
-        "var*2-var" =>  "var*2-var",
-        "var/2+var" => "var/2+var",
-        "var-2+4+var" =>  "var+2+var",
-        "var-2*4*var" =>  "var+-8*var",
-        #************************************************HIGH SCORE LINE******************************************************
-        "var/2*4+var" =>  "var*2+var",
-        "1+var/2*4+var" =>  "1+var*2+var"
-    ]
-    test_hash[0].each_with_index do |test, index|
-      if index < cleared then regression_tests[test[0]] = test[1]
-      else dev_tests[test[0]] = test[1]
-      end
-    end
-    tests = (regression_tests.any? ? dev_tests.merge(regression_tests) : dev_tests)
-    tests.each_with_index do |(key, value), index|
-      result = Dentaku.evaluate(key, {}).to_s
-      test_type = index < (tests.size-cleared) ? 'dev' : 'regression'
-      STDERR.puts "  passed #{test_type} test #{index+1}: '#{key}'; answer is '#{value}'" if value == result
-      STDERR.puts "\nFAILED #{test_type} test #{index+1}: '#{key}'; '#{result}' was incorrect; answer is '#{value}'" unless value == result
-    end
-  end
-end
-
-Test.start
-
-
-#test_str = ("10 / 5 - var0**(2-4+7*x) >= 3 && !(var1 ? true : false || var2 != false)")
 
 "#{
 def test
